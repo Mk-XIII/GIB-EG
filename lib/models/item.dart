97d1bb82867c322ae77d4b4a0fd9678a8718cccc
Elis Merenianu <elis.merenianu@ktu.edu>
2021-04-01 00:53:51 +0300

    Added item constructor

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -8,6 +8,16 @@ class Item {
   String quality;
   int value;
 
+  Item(String name, String description, String sprite, String rarity,
+      String quality, int value) {
+    this.name = name;
+    this.description = description;
+    this.sprite = sprite;
+    this.rarity = rarity;
+    this.quality = quality;
+    this.value = value;
+  }
+
   void displayItem() {
     print("Info: " + quality + " " + name + " - " + description);
   }
