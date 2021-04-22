a7214a0c64340d4ca24bcbe780c5cdab33198a4c
Arnas Bagocius <abagocius0@gmail.com>
2021-04-22 11:08:52 +0300

    replace booster sprite to real images

--- lib/models/booster.dart
+++ lib/models/booster.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:GIB_EG/presentation/eg_cons_icons.dart';
 import 'package:flutter/widgets.dart';
 
@@ -5,21 +7,24 @@ class Booster {
   String name;
   String description;
   int price;
-  IconData icon;
   String sprite;
 
-  Booster(String name, String description, int price, IconData icon) {
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.icon = icon;
-  }
+  Booster(this.name, this.description, this.price, this.sprite);
+
+  Booster.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        description = json['description'],
+        price = json['price'],
+        sprite = json['sprite'];
 }
 
 final List<Booster> boosters = [
-  Booster("Whetstone", "+% to get a better quality item", 100, EgCons.profile),
-  Booster("Yellow spray can", "+% to sold item value", 200, EgCons.achievement),
-  Booster("Magnifying glass", "+% to get a better item", 300, EgCons.currency),
-  Booster("Broken mirror", "+% to get two items instead of one", 400, EgCons.champion_cup),
-  Booster("Golden feather", "Clicking eg generates gold", 500, EgCons.settings),
+
+
+  Booster("Whetstone", "+% to get a better quality item", 100, "assets/boosters/whetstone.png"),
+  Booster("Yellow spray can", "+% to sold item value", 200, "assets/boosters/spray_booster.png"),
+  Booster("Magnifying glass", "+% to get a better item", 300, "assets/boosters/magnifying_glass.png"),
+  Booster("Broken mirror", "+% to get two items instead of one", 400,
+      "assets/boosters/mirror_booster.png"),
+  Booster("Golden feather", "Clicking eg generates gold", 500, "assets/boosters/feather.png"),
 ];
